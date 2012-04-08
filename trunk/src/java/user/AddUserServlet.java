215777df03143d59e50f7ecbbd80ff84080d0b96
jason.chokong.wong <jason.chokong.wong@f8168a3b-d330-90e1-6dc7-0e9b6d4529ff>
2012-04-08 08:06:23 +0000

    Changed the registration so that it send an email to the user so that they would need to click on the link to activate account.

--- trunk/src/java/user/AddUserServlet.java
+++ trunk/src/java/user/AddUserServlet.java
@@ -8,36 +8,85 @@ import business.User;
 import data.UserDB;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.mail.MessagingException;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import data.MailUtilGmail;
+        
 /**
  *
  * @author Kyle
  */
 public class AddUserServlet extends HttpServlet {
 
+    
+    private void sendEmail(String urlcode, String username, String emailAddress){
+    // send email to user
+        String to = emailAddress;
+        String from = "reviewsite.info3120@gmail.com";
+        String subject = "Welcome to our Review Movie Site";
+        String body = "Dear " + username + ",\n\n" +
+            "Thanks for joining our Review Movie Site. " + "\n\n" +
+                "Please visit the following link to finish the registration process\n\n" +
+                "http://localhost:8080/ReviewSite/ComfirmNewUser?urlcode=" +
+                urlcode +
+                "\n" +
+                "Have a great day and thanks again!\n\n" +
+                "Review Movie Site\n";
+        boolean isBodyHTML = false;
+
+        try
+        {
+            //MailUtilLocal.sendMail(to, from, subject, body, isBodyHTML);
+            MailUtilGmail.sendMail(to, from, subject, body, isBodyHTML);
+        }
+        catch (MessagingException e)
+        {
+            String errorMessage = 
+                "ERROR: Unable to send email. " + 
+                    "Check Tomcat logs for details.<br>" +
+                "NOTE: You may need to configure your system " + 
+                    "as described in chapter 15.<br>" +
+                "ERROR MESSAGE: " + e.getMessage();
+            //request.setAttribute("errorMessage", errorMessage);
+            this.log(
+                "Unable to send email. \n" +
+                "Here is the email you tried to send: \n" +
+                "=====================================\n" +
+                "TO: " + emailAddress + "\n" +
+                "FROM: " + from + "\n" +
+                "SUBJECT: " + subject + "\n" +
+                "\n" +
+                body + "\n\n");
+        }
+    }
+    
     protected void doPost(HttpServletRequest request, 
             HttpServletResponse response) 
             throws ServletException, IOException
     {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-       
-        HttpSession session = request.getSession();
-
-        // TODO: add code that gets the User object from the session and updates the database
         String emailAddress = request.getParameter("emailAddress");
 
-        User user = new User();
-        user.setUserName(username);
-        user.setPassword(password);
-        user.setEmailAddress(emailAddress);
-        UserDB.insert(user);
+        //insert data to pending user table
+        String urlcode = UserDB.insertPending(username, password, emailAddress);
+               
+        //email link to user
+        sendEmail(urlcode, username, emailAddress);
+        
+       
+//        User user = new User();
+//        user.setUserName(username);
+//        user.setPassword(password);
+//        user.setEmailAddress(emailAddress);
+//        UserDB.insert(user);
                
         String url = "/index.jsp";
         RequestDispatcher dispatcher =
